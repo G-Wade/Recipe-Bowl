3c0c12310e26bba989276b28553036b789e94b1c
G-Wade <gareth.wade@btinternet.com>
2023-01-26 17:47:11 +0000

    ION test

--- app/src/main/java/com/example/recipebowl/HomeActivity.kt
+++ app/src/main/java/com/example/recipebowl/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.example.recipebowl
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.koushikdutta.ion.Ion
+import org.json.JSONObject
+
+private val Nutella = "https://world.openfoodfacts.org/api/v0/product/3017620422003.json"
 
 class HomeActivity : AppCompatActivity() {
 
@@ -10,7 +15,17 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val Button = findViewById<Button>(R.id.Button)
+
         val mainToolbar = findViewById<Toolbar>(R.id.main_toolbar)
         setSupportActionBar(mainToolbar)
+
+        Ion.with(this).load(Nutella).setHeader("User-Agent", "Gareth Wade")
+            .setHeader("Accept", "application/JSON").asString()
+            .setCallback { _, result ->
+                val jsonObj = JSONObject(result)
+                val value = jsonObj.getString("code")
+                Button.text = value
+            }
     }
 }
