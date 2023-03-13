5efc8d371e2f76559ea0552d2d4151bcf3a51ad5
G-Wade <gareth.wade@btinternet.com>
2023-03-13 13:19:04 +0000

    Messing with Fragments

--- app/src/main/java/com/example/recipebowl/HomeActivity.kt
+++ app/src/main/java/com/example/recipebowl/HomeActivity.kt
@@ -2,56 +2,26 @@ package com.example.recipebowl
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.firestore.FirebaseFirestore
-import com.koushikdutta.ion.Ion
-import org.json.JSONObject
 import android.util.Log
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.QuerySnapshot
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
-    private lateinit var user : FirebaseUser
-    private lateinit var db : FirebaseFirestore
 
     //private val Nutella = "https://world.openfoodfacts.org/api/v0/product/3017620422003.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val profileFragment = ProfileFragment()
-
-        setCurrentFragment(profileFragment)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.profileNav -> setCurrentFragment(profileFragment)
-            }
-            true
-        }
 
         auth = FirebaseAuth.getInstance()
         if (auth.currentUser == null) {
@@ -60,16 +30,22 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
+        val browseFragment = BrowseFragment()
+        val profileFragment = ProfileFragment()
+
+        //setCurrentFragment(browseFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                //R.id.browseNav -> setCurrentFragment(browseFragment)
+                R.id.profileNav -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
         val mainToolbar = findViewById<Toolbar>(R.id.main_toolbar)
         setSupportActionBar(mainToolbar)
-
-        db = FirebaseFirestore.getInstance()
-
-        db.collection("Recipe").get().addOnCompleteListener {
-            if (it.isSuccessful) {
-                 populateHome(it.result)
-            }
-        }
 
         /*
         Ion.with(this).load(Nutella).setHeader("User-Agent", "Gareth Wade")
