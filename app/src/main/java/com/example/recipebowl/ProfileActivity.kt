d5b265eb78ea113ef8de6e479a7f47d3dbcbd642
G-Wade <gareth.wade@btinternet.com>
2023-03-14 23:59:38 +0000

    Profile username

--- app/src/main/java/com/example/recipebowl/ProfileActivity.kt
+++ app/src/main/java/com/example/recipebowl/ProfileActivity.kt
@@ -3,20 +3,25 @@ package com.example.recipebowl
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.TextViewCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigationView.selectedItemId = R.id.profileNav
@@ -49,5 +54,13 @@ class ProfileActivity : AppCompatActivity() {
 
         val mainToolbar = findViewById<Toolbar>(R.id.main_toolbar)
         setSupportActionBar(mainToolbar)
+
+        val username = findViewById<TextView>(R.id.username)
+        val docRef = db.collection("User").document(auth.uid.toString())
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null) {
+                username.text = document.getString("username")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/recipebowl/RegisterActivity.kt
+++ app/src/main/java/com/example/recipebowl/RegisterActivity.kt
@@ -12,17 +12,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     //UI elements
     private lateinit var emailText : EditText
     private lateinit var passwordText : EditText
+    private lateinit var usernameText : EditText
     private lateinit var regBtn : Button
     private lateinit var logoutBtn : Button
     private lateinit var loginText : TextView
-
-    private lateinit var auth : FirebaseAuth
 
     override fun onStart() {
         super.onStart()
@@ -43,9 +46,11 @@ class RegisterActivity : AppCompatActivity() {
         setSupportActionBar(mainToolbar)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
         emailText = findViewById(R.id.emailText)
         passwordText = findViewById(R.id.passwordText)
+        usernameText = findViewById(R.id.usernameText)
         regBtn = findViewById(R.id.registerBtn)
         logoutBtn = findViewById(R.id.logoutBtn)
         loginText = findViewById(R.id.loginLink)
@@ -57,6 +62,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun registerClick() {
         val email = emailText.text.toString()
         val password = passwordText.text.toString()
+        val username = usernameText.text.toString()
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show()
@@ -68,11 +74,32 @@ class RegisterActivity : AppCompatActivity() {
             return
         }
 
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Enter Username", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener() { task ->
             if (task.isSuccessful) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d(TAG, "createUserWithEmail:success")
                 Toast.makeText(this, "Authentication success.", Toast.LENGTH_SHORT).show()
+
+                val map = hashMapOf("e-mail" to email, "username" to username)
+                val uid = auth.uid.toString()
+                val docRef = db.collection("User").document(uid)
+
+                docRef.set(map)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("Success", "Success adding document")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("Failure", "Error adding document", e)
+                    }
+
+                val newIntent = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(newIntent)
+                finish()
             } else {
                 // If sign in fails, display a message to the user.
                 Log.w(TAG, "createUserWithEmail:failure", task.exception)
